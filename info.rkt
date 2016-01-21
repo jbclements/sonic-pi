658069a6139795f87bf317134adeaa04201c49d4
John Clements <clements@racket-lang.org>
2016-01-20 22:02:01 -0800

    updated deps

--- info.rkt
+++ info.rkt
@@ -5,10 +5,10 @@
 (define deps
   (list "base"
         "osc"
-        #;(
         "rackunit-lib"
-        "scribble-lib"
         "typed-racket-lib"
+        #;(
+        "scribble-lib"
         "typed-racket-more"
         "unstable-contract-lib"
         "web-server-lib")))
