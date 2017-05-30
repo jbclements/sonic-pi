42e7a678e1eca7aa86a3ac5adfa7376019533dee
sparksaus <sparksaus@gmail.com>
2017-05-30 03:29:22 -0700

    fixed example

--- sonic-pi/examples/lsonic-live-loop-example.rkt
+++ sonic-pi/examples/lsonic-live-loop-example.rkt
@@ -1,6 +1,6 @@
 #lang s-exp sonic-pi/lsonic
 
-#;(live_loop "boom"
+(live_loop "boom"
            (block
             (fx "reverb" "room" 1
                 (block (sample "bd_boom"
@@ -10,20 +10,21 @@
                                "amp" 10
                                "rate" 1)
                        (psleep 8)))))
-#;(live_loop "guit"
+(live_loop "guit"
            (block (fx "echo" "mix" 0.3 "phase" 0.25
                       (block (sample "guit_em9" "rate" 0.5)
                              (sample "guit_e_fifths" "rate" 0.5)
                              (psleep 8)))))
 
 ;; Wob rhythm from sonic-pi
-(live_loop "drum"
-           (fx "wobble" "phase" 2
-               (block (fx "echo" "mix" 0.6
-                          (block (sample "drum_heavy_kick")
-                                 (sample "bass_hit_c" "rate" 0.8 "amp" 0.4)
-                                 (psleep 1))))))
-(live_loop "choir"
+#;(live_loop "drum"
+           (block
+            (fx "wobble" "phase" 2
+                (block (fx "echo" "mix" 0.6
+                           (block (sample "drum_heavy_kick")
+                                  (sample "bass_hit_c" "rate" 0.8 "amp" 0.4)
+                                  (psleep 1)))))))
+#;(live_loop "choir"
            (block (fx "reverb"
                       (block (sample "ambi_choir"
                                      "rate" (choose 0.5 (/ 1.0 3) (/ 3.0 5))
